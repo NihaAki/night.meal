41b0bb4549fb9f0e3479336636f65749020f8bff
NihaAki <89088712+NihaAki@users.noreply.github.com>
2021-08-22 00:15:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,36 @@ let index = 0;
 while(index < 5){
 //繰り返したい命令
 index++;//←これは絶対必要
-}
+};
 
 
 console.document='abc'
+
+const konnichiha = (arg) => {
+if (abc > arg) {
+
+    console.log('これはサンプル１');
+} else {
+    console.log('これはサンプル４４')
+}
+};
+
+konnichiha(4);
+
+console.log(sample4.color);
+
+//オブジェクト
+const sample4 ={
+    color:'pink',
+    size:'large',
+    aisatsu: () =>{
+            console.log('aisatsu');
+
+    }
+};
+
+console.log(window)
+
+console.log(document)
+
+
