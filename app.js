3475923eed8fb34602f349b051d9f8c53b10027c
NihaAki <89088712+NihaAki@users.noreply.github.com>
2021-09-08 15:27:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,73 @@
+// let abc = 'このページは大切です';
 
-const para = document.querySelector('p');
-
-para.addEventListener('click', updateName);
-
-function updateName() {
-  let name = prompt('名前を入力して下さい');
-  para.textContent = 'Player 1: ' + name;
-}
-// 上サンプルジャバ
-
-let abc = 'このページは大切です';
-
-const aiu ='私は動かせる存在ではありません。';
+// const aiu ='私は動かせる存在ではありません。';
 
 
-let index = 0;
-while(index < 5){
-//繰り返したい命令
-index++;//←これは絶対必要
-};
+// let index = 0;
+// while(index < 5){
+// //繰り返したい命令
+// index++;//←これは絶対必要
+// };
 
 
-console.document='abc'
+// console.document='abc'
 
-const konnichiha = (arg) => {
-if (abc > arg) {
+// const konnichiha = (arg) => {
+// if (abc > arg) {
 
-    console.log('これはサンプル１');
-} else {
-    console.log('これはサンプル４４')
-}
-};
+//     console.log('これはサンプル１');
+// } else {
+//     console.log('これはサンプル４４')
+// }
+// };
 
-konnichiha(4);
+// konnichiha(4);
 
-const sample4 ={
-    color:'pink',
-    size:'large',
-    aisatsu: () =>{
-            console.log('aisatsu');
+// const sample4 ={
+//     color:'pink',
+//     size:'large',
+//     aisatsu: () =>{
+//             console.log('aisatsu');
 
 
-console.log(sample4.color);
+// console.log(sample4.color);
 
-//オブジェクト
+// //オブジェクト
 
+//     }
+// };
+
+// console.log(window)
+
+// console.log(document)
+
+// console.log(document.getElementsByName('buttton')[0])
+
+// document-getElementsByName('button')[0]-addEventListener('click', ()=> {
+
+//     window.alert('これは作動していますか？');
+// });
+
+
+window.onload = function(){
+    var dateObj = new Date();
+    var y = dateObj.getFullYear();
+    var m = dateObj.getMonth() + 1;
+    var d = dateObj.getDate();
+    var yb = "日月火水木金土".charAt(dateObj.getDay());
+    document.getElementById("currentDate").innerHTML = y+"年"+m+"月"+d+"日("+yb+")";
     }
-};
 
-console.log(window)
+window.onload = function(){
+var jikan = new Date();
+var h = jikan.gethours();
+var mi = jikan.getminutes();
+var se = jikan.getsecond();
 
-console.log(document)
+document.getElementById("currenttime").innerHTML=h+mi+se;
 
-console.log(document.getElementsByName('buttton')[0])
+}
 
-document-getElementsByName('button')[0]-addEventListener('click', ()=> {
-
-    window.alert('これは作動していますか？');
-});
-
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+window.onload = function(){
+alert("ページの更新が完了しました");
+}
